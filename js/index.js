4144808ad641166c2572ef171d6aca712fddf483
Connor55Jackson <43474688+Connor55Jackson@users.noreply.github.com>
2018-09-23 11:43:18 -0400

    Create index.js
    Copy

---
+++ js/index.js
@@ -0,0 +1,25 @@
+var listItems = document.getElementsByTagName('li');
+listItems[0].addEventListener('click',function(){
+  window.open("mod1.html","_blank")
+});
+listItems[1].addEventListener('click',function(){
+  window.open("mod2.html","_blank")
+});
+listItems[2].addEventListener('click',function(){
+  window.open("mod3.html","_blank")
+})
+listItems[3].addEventListener('click',function(){
+  window.open("mod4.html","_blank")
+})
+listItems[4].addEventListener('click',function(){
+  window.open("mod5.html","_blank")
+});
+listItems[5].addEventListener('click',function(){
+  window.open("mod6.html","_blank")
+});
+listItems[6].addEventListener('click',function(){
+  window.open("mod7.html","_blank")
+})
+listItems[7].addEventListener('click',function(){
+  window.open("mod8.html","_blank")
+})
